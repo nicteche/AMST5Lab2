2b0a144faacbd3c266c41577198f30346531518d
gersonyuquilema <gersonyuquilema@outlook.com>
2022-11-03 17:41:58 -0500

    Commit 3er modificacion

--- Lab2/app/src/main/java/com/example/amst_grupo5_lab2_cloud_firebase/MainActivity.java
+++ Lab2/app/src/main/java/com/example/amst_grupo5_lab2_cloud_firebase/MainActivity.java
@@ -23,13 +23,16 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import java.util.HashMap;
+
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
+
     GoogleSignInClient mGoogleSignInClient;
-    ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new
-            ActivityResultCallback<ActivityResult>() {
+    ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
                 public void onActivityResult(ActivityResult result) {
                     if (result.getResultCode() == Activity.RESULT_OK) {
                         Intent intent = result.getData();
@@ -43,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,23 +59,25 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
+
     public void iniciarSesion(View view) {
         resultLauncher.launch(new Intent(mGoogleSignInClient.getSignInIntent()));
     }
+
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
         Log.d("TAG", "firebaseAuthWithGoogle:" + acct.getId());
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
-        .addOnCompleteListener(this, task -> {
-            if (task.isSuccessful()) {
-                FirebaseUser user = mAuth.getCurrentUser();
-                updateUI(user);
-                } else {
-                System.out.println("error");
-                updateUI(null);
-                }
-            });
-        }
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
+                    } else {
+                        System.out.println("error");
+                        updateUI(null);
+                    }
+                });
+    }
 
     private void updateUI(FirebaseUser user) {
         if (user != null) {
@@ -80,5 +86,22 @@ public class MainActivity extends AppCompatActivity {
             String photo = String.valueOf(user.getPhotoUrl());
         } else {
             System.out.println("sin registrarse");
-            } }
+        }
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            HashMap<String, String> info_user = new HashMap<String, String>();
+            info_user.put("user_name", user.getDisplayName());
+            info_user.put("user_email", user.getEmail());
+            info_user.put("user_photo", String.valueOf(user.getPhotoUrl()));
+            info_user.put("user_id", user.getUid());
+            finish();
+            Intent intent = new Intent(this, PerfilUsuario.class);
+            intent.putExtra("info_user", info_user);
+            startActivity(intent);
+        } else {
+            System.out.println("sin registrarse");
+        }
+    }
 }
---
+++ Lab2/app/src/main/java/com/example/amst_grupo5_lab2_cloud_firebase/PerfilUsuario.java
@@ -0,0 +1,43 @@
+package com.example.amst_grupo5_lab2_cloud_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.HashMap;
+
+public class PerfilUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_usuario);
+    TextView txt_id, txt_name, txt_email;
+    ImageView imv_photo;
+        Intent intent = getIntent();
+        HashMap<String, String> info_user = (HashMap<String, String>)intent.getSerializableExtra("info_user");
+        txt_id = findViewById(R.id.txt_userId);
+        txt_name = findViewById(R.id.txt_nombre);
+        txt_email = findViewById(R.id.txt_correo);
+        imv_photo = findViewById(R.id.imv_foto);
+        txt_id.setText(info_user.get("user_id"));
+        txt_name.setText(info_user.get("user_name"));
+        txt_email.setText(info_user.get("user_email"));
+        String photo = info_user.get("user_photo");
+        Picasso.with(getApplicationContext()).load(photo).into(imv_photo);
+    }
+    public void cerrarSesion(View view){
+        FirebaseAuth.getInstance().signOut();
+        finish();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("msg", "cerrarSesion");
+        startActivity(intent);
+    }
+
+}
