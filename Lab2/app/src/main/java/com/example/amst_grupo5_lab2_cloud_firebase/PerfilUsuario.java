741dfccf20301d671fd9b13646c311565e66f838
nicteche <nicteche@espol.edu.ec>
2022-11-03 23:00:02 -0500

    Ultima actualizacion

--- Lab2/app/src/main/java/com/example/amst_grupo5_lab2_cloud_firebase/PerfilUsuario.java
+++ Lab2/app/src/main/java/com/example/amst_grupo5_lab2_cloud_firebase/PerfilUsuario.java
@@ -5,9 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
